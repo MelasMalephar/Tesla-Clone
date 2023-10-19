1b418ed15d39f26558b41c78c11f83b69dcf857a
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2023-10-19 16:10:01 +0530

    Update Home.js
    images

--- src/Home.js
+++ src/Home.js
@@ -8,49 +8,49 @@ function Home() {
             <Section
                 title="Model S"
                 description="Order Online For Touchless delivery"
-                backgroundImg="model-s.jpg"
+                backgroundImg="/images/model-s.jpg"
                 leftbtnText="Custom order"
                 rightbtnText="Existing inventory"
             />
             <Section
                  title="Model Y"
                  description="Order Online For Touchless delivery"
-                 backgroundImg="model-y.jpg"
+                 backgroundImg="/images/model-y.jpg"
                  leftbtnText="Custom order"
                  rightbtnText="Existing inventory"
             />
             <Section
                  title="Model 3"
                  description="Order Online For Touchless delivery"
-                 backgroundImg="model-3.jpg"
+                 backgroundImg="/images/model-3.jpg"
                  leftbtnText="Custom order"
                  rightbtnText="Existing inventory"
             />
             <Section
                  title="Model X"
                  description="Order Online For Touchless delivery"
-                 backgroundImg="model-x.jpg"
+                 backgroundImg="/images/model-x.jpg"
                  leftbtnText="Custom order"
                  rightbtnText="Existing inventory"
             />
             <Section
                 title="Lowest Cost Solar Panel in America"
                 description="Money-back guarantee"
-                backgroundImg="solar-panel.jpg"
+                backgroundImg="/images/solar-panel.jpg"
                 leftbtnText="Order now"
                 rightbtnText="Learn More"
             />
             <Section
                 title="Solar for New Roofs"
                 description="Solar Roof Costs Less Than a New Roof Plus Solar Panel"
-                backgroundImg="solar-roof.jpg"
+                backgroundImg="/images/solar-roof.jpg"
                 leftbtnText="Order now"
                 rightbtnText="Learn More"
             /> 
             <Section
                 title="Accessories"
                 description=""
-                backgroundImg="accessories.jpg"
+                backgroundImg="/images/accessories.jpg"
                 leftbtnText="Shop now"
                 rightbtnText=""
             />
